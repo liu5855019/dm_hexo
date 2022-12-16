1f9cb0df809823403d00214684d48ce67715bd15
daimu <jinlin@wangdou-hbean.com>
2022-12-16 15:18:07 +0800

    add publish.ps1

--- public.sh
+++
@@ -1,3 +0,0 @@
-
-echo "start public";
-pwd
---
+++ publish.ps1
@@ -0,0 +1,19 @@
+
+Get-Location
+
+Write-Output "start public";
+
+hexo g
+
+
+Write-Output "start cp";
+Copy-Item .\public\* ..\liu5855019.github.io -Recurse -Force;
+Write-Output "end cp";
+
+Set-Location ..\liu5855019.github.io
+Get-Location
+
+git status
+git add .
+git commit . -m "update"
+

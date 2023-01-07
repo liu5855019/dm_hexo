d05d0b0d55314b6e31c52d8b69502cd2d36d6c2a
daimu <258141764@qq.com>
2023-01-07 16:33:13 +0800

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -12,12 +12,3 @@ echo "start cp";
 \cp -R -f ./public/ ../liu5855019.github.io/
 echo "end cp";
 
-
-
-# Set-Location ..\liu5855019.github.io
-# Get-Location
-
-# git status
-# git add .
-# git commit -a -m "update"
-
